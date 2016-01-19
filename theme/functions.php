22fff2e981cc212de963e5270089d6fb9028e624
Björn Rikte <bjorn.rikte@telia.com>
2016-01-19 01:06:42 +0100

    Added dynamic navbar

--- theme/functions.php
+++ theme/functions.php
@@ -15,5 +15,20 @@ function get_title($title) {
   return $title . (isset($branax['title_append']) ? $branax['title_append'] : null);
 }
 
+/**
+ * Create a navigation bar / menu for the site.
+ *
+ * @param string $menu for the navigation bar.
+ * @return string as the html for the menu.
+ */
+function get_navbar($menu) {
+  $html = "<nav>\n<ul class='{$menu['class']}'>\n";
+  foreach($menu['items'] as $item) {
+    $selected = $menu['callback_selected']($item['url']) ? " class='selected' " : null;
+    $html .= "<li{$selected}><a href='{$item['url']}' title='{$item['title']}'>{$item['text']}</a></li>\n";
+  }
+  $html .= "</ul>\n</nav>\n";
+  return $html;
+}
 
 
